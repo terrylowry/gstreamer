9d22fbc1089f9556de8e06cf30a87793df7861b0
Daniel Morin <daniel.morin@collabora.com>
2025-06-03 22:32:17 -0400

    gststructure: add gst_structure_get_caps
    - Get field of type #GstCaps from #GstStructure
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9184>

--- subprojects/gstreamer/gst/gststructure.h
+++ subprojects/gstreamer/gst/gststructure.h
@@ -31,6 +31,7 @@ G_BEGIN_DECLS
 GST_API GType _gst_structure_type;
 
 typedef struct _GstStructure GstStructure;
+typedef struct _GstCaps GstCaps;
 
 /**
  * GST_SERIALIZE_FLAG_STRICT:
@@ -413,6 +414,10 @@ gboolean              gst_structure_can_intersect (const GstStructure * struct1,
 GST_API
 GstStructure *        gst_structure_intersect     (const GstStructure * struct1,
                                                    const GstStructure * struct2) G_GNUC_MALLOC;
+GST_API
+gboolean              gst_structure_get_caps      (const GstStructure * structure,
+                                                   const gchar * fieldname,
+                                                   const GstCaps ** caps);
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(GstStructure, gst_structure_free)
 
